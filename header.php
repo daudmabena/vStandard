43e5c427156c756ab2b56ae1b7733a4b5f2dc94b
Rory Standley <rorystandley@gmail.com>
2013-03-01 16:01:17 +0000

    Setup index.php
    Setup template for index.php. Added in template-tags and content-aside
    for use with posts and comments.

--- header.php
+++ header.php
@@ -43,11 +43,11 @@
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<!--[if lt IE 9]>
-		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script><!-- For all versions of IE less than 9. This will allow them to render HTML5 Elements correctly -->
+		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 		<![endif]-->
 		<?php wp_head(); ?>
-		</head>
-    <body <?php body_class(); ?>>
+	</head>
+    <body <?php //body_class(); ?>>
     	<div id="page" class="hfeed site">
      		<header id="masthead" class="site-header" role="banner">
          		<hgroup>
