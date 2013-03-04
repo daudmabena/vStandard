13f4a5d9418ffa85069103e20ff393f28067dae2
Rory Standley <rorystandley@gmail.com>
2013-03-04 14:57:29 +0000

    Sidebar and Footer
    Sidebar and Footer templates created. Credits added to Footer

--- footer.php
+++ footer.php
@@ -1,8 +1,24 @@
-			
-			</div><!-- #main .site-main -->
-     		<footer id="colophon" class="site-footer" role="contentinfo">
-        		<div class="site-info"></div><!-- .site-info -->
-     		</footer><!-- #colophon .site-footer -->
-		</div><!-- #page .hfeed .site -->
-	</body>
+<?php
+/**
+* The template for displaying the footer.
+*
+* Contains the closing of the id=main div and all content after
+*
+* @package vStandard
+* @since vStandard 1.0
+*/
+?>
+ 
+</div><!-- #main .site-main -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+        <?php do_action('vstandard_credits'); ?>
+        <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'vstandard' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'vstandard'), 'WordPress'); ?></a>
+        <span class="sep"> | </span>
+        <?php printf( __('Theme: %1$s by %2$s.', 'vstandard' ), 'vstandard', '<a href="http://www.rstandley.co.uk/" rel="designer">Theme vStandard</a>'); ?>
+    </div><!-- .site-info -->
+</footer><!-- #colophon .site-footer -->
+</div><!-- #page .hfeed .site -->
+<?php wp_footer(); ?>
+</body>
 </html>
